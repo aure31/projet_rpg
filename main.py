af5d75329aec451e6a8e26581af6bf37d30f3e21
RpgMaker ProGamer <GamerMakerRpgPro@gmail.com>
2024-09-21 22:00:08 +0200

    implementation d'une fenetre à taille changable

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 import pygame as p
+import tools as t
 
 print('start')
 
 p.init()
 p.font.init()
-FPS = 60
-WIDTH, HEIGHT = 900, 500
-WIN = p.display.set_mode((WIDTH, HEIGHT))
+FPS = 30
+flags = p.RESIZABLE
+#crée une fenetre de taille (900,500)px dont on peut modifier la taille
+WIN = p.display.set_mode((900, 500),flags,vsync=1 )
 BACKGROUND = (28, 22, 79)
 
 clock = p.time.Clock()
 p.display.set_caption('Hello World!')
 
 def main():
+	#taille de l'ecran
+	WIDTH , HEIGHT = WIN.get_size()
 	while True:
     	# Process player inputs.
+		# event (evenement qui se passe)
 		for event in p.event.get():
 			if event.type == p.QUIT:
 				p.quit()
 				raise SystemExit
+			#quand la taille est changer
+			if event.type == p.VIDEORESIZE :
+				WIDTH ,HEIGHT = WIN.get_size()
 
     	# Do logical updates here.
     	# ...
 
+		#colorie le fond en violet
 		WIN.fill("purple")  # Fill the display with a solid color
-
+		#crée un text puis l'affiche
+		tex = t.texte(["je t'aime mon bb"],WIDTH/2,HEIGHT/2,90,(255, 135, 247))
+		tex.affiche(WIN)
     	# Render the graphics here.
     	# ...
 
 		p.display.flip()  # Refresh on-screen display
-		clock.tick(60)   
+		#permet de faire attendre la boucle pour attaindre FPS par second
+		clock.tick(FPS)   
 
   
 
