985ef5091e2c4f460326b6550dd4835a9bba9daa
kedor <99351777+aure31@users.noreply.github.com>
2024-09-25 16:03:30 +0200

    grosse refonte des fichier

--- client/main.py
+++ client/main.py
@@ -1,5 +1,6 @@
 import pygame as p
 import tools as t
+import os
 
 print('start')
 
@@ -9,21 +10,24 @@ FPS = 30
 flags = p.RESIZABLE
 #crée une fenetre de taille (900,500)px dont on peut modifier la taille
 WIN = p.display.set_mode((900, 500),flags)
-global WIDTH 
-global HEIGHT
-WIDTH ,HEIGHT = WIN.get_size()
 BACKGROUND = (28, 22, 79)
-bg = p.image.load("Assets\\grassbg.jpg")
+bg = p.image.load(os.path.join('Assets', 'grassbg.jpg'))
 
 
 clock = p.time.Clock()
 p.display.set_caption('Hello World!')
 
+def test():
+	print("test")
+
 def main():
 	#taille de l'ecran
-	 
+	global WIDTH
+	global HEIGHT
+	WIDTH ,HEIGHT = WIN.get_size()
+	localBG = p.transform.scale(bg,(WIDTH,HEIGHT))
 	while True:
-    	# Process player inputs.
+		# Process player inputs.
 		# event (evenement qui se passe)
 		for event in p.event.get():
 			if event.type == p.QUIT:
@@ -33,20 +37,20 @@ def main():
 			if event.type == p.VIDEORESIZE :
 				print("resize")
 				WIDTH ,HEIGHT = WIN.get_size()
-				p.transform.scale(bg,(WIDTH,HEIGHT))
+				localBG = p.transform.scale(bg,(WIDTH,HEIGHT))
 				print("resize2")
 
-    	# Do logical updates here.
-    	# ...
+		# Do logical updates here.
+		# ...
 
 		#colorie le fond en violet
-		WIN.blit(bg,(0,0))  # Fill the display with a solid color
+		#WIN.fill("violet")
+		WIN.blit(localBG,(0,0))  # Fill the display with a solid color
 		#crée un text puis l'affiche
-		print(p.display.get_surface())
-		tex = t.texte(["je t'aime mon bb"],WIDTH/2,HEIGHT/2,90,(255, 135, 247))
+		tex = t.texte(["je t'aime mon bb"],WIDTH/2,HEIGHT/2,WIDTH//10,(255, 135, 247))
 		tex.affiche(WIN)
-    	# Render the graphics here.
-    	# ...
+		# Render the graphics here.
+		# ...
 
 		p.display.flip()  # Refresh on-screen display
 		#permet de faire attendre la boucle pour attaindre FPS par second
